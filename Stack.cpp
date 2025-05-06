c56f525c198e5113699c3959872f30ea44594a5a
Oleksandra <syatle13@gmail.com>
2025-05-06 19:42:59 +0300

    Add project files.

---
+++ Stack.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+class Stack
+{
+	// Нижняя и верхняя границы стека
+	enum { EMPTY = -1, FULL = 20 };
+	// Массив для хранения данных
+	char st[FULL + 1];////////////////////////data
+
+	////  Person* arr=new Person[FULL ];
+	/////   int * arr=new int[FULL];
+
+	// Указатель на вершину стека
+	int top;
+
+public:
+	// Конструктор
+	Stack();
+
+	// Добавление элемента
+	void Push(char c);
+
+	// Извлечение элемента
+	char Pop();
+
+	// Очистка стека
+	void Clear();
+
+	// Проверка существования элементов в стеке
+	bool IsEmpty();
+
+	// Проверка на переполнение стека
+	bool IsFull();
+
+	// Количество элементов в стеке
+	int GetCount();
+
+	void Output()
+	{
+		for (int i = 0;i < top + 1;i++)
+		{
+			cout << st[i] << " ";
+		}
+		cout << endl;
+	}
+};
+
+
+Stack::Stack()
+{
+	// Изначально стек пуст
+	top = EMPTY;
+}
+
+void Stack::Clear()
+{
+	// Эффективная "очистка" стека 
+	// (данные в массиве все еще существуют, 
+	// но функции класса, ориентированные на работу с вершиной стека,
+	// будут их игнорировать)
+	top = EMPTY;
+}
+
+bool Stack::IsEmpty()
+{
+	// Пуст?
+	return top == EMPTY;
+}
+
+bool Stack::IsFull()
+{
+	// Полон?
+	return top == FULL;
+}
+
+int Stack::GetCount()
+{
+	// Количество присутствующих в стеке элементов
+	return top + 1;
+}
+
+void Stack::Push(char c)
+{
+	// Если в стеке есть место, то увеличиваем указатель
+	// на вершину стека и вставляем новый элемент
+	if (!IsFull())
+		st[++top] = c;
+}
+
+char Stack::Pop()
+{
+	// Если в стеке есть элементы, то возвращаем верхний и
+	// уменьшаем указатель на вершину стека
+	if (!IsEmpty())
+		return st[top--];
+	else // Если в стеке элементов нет
+		return 0;
+}
+
+void main()
+{
+
+	int a;
+	int b;
+	int c;
+
+
+	srand(time(0));
+	Stack ST;
+	char c;
+	// пока стек не заполнится
+	while (!ST.IsFull()) 
+	{
+		c = rand() % 4 + 2;
+		ST.Push(c);
+	}
+	ST.Output();
+	// пока стек не освободится
+	int i = 0;
+	while (i<5) 
+	{
+		c = ST.Pop();
+		cout << c << " ";
+		i++;
+	}
+	cout << "\n\n";
+	ST.Output();
+	
+
+	system("pause");
+}
+
